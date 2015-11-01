5446320c2d197744f94fc5f62a4d64b1f9df2926
Victor Menezes <menezes.victor@gmail.com>
2015-11-01 18:35:18 -0500

    changed serializer to process POST

--- manytomany_proj/app/serializers.py
+++ manytomany_proj/app/serializers.py
@@ -13,3 +13,11 @@ class ArticleSerializer(ModelSerializer):
     class Meta:
         model = Article
         fields = ('headline', 'publications')
+    
+    def create(self, validated_data):
+        publication_data = validated_data.pop('publications')
+        article = Article.objects.create(**validated_data)
+        for i in publication_data:
+            publication = Publication.objects.create(**i)
+            article.publications.add(publication)
+        return article
